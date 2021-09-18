20298001a89b7a7cfffdcd8b698a444e1ad916d5
JaviMY <javier@masteryield.com>
2021-09-18 22:06:08 +0200

    cdcdcddc

--- server.js
+++ server.js
@@ -1,35 +1,11 @@
-// server.js
-
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-// ^ middleware to redirect all URLs to index.html
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
 
 const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(staticFileMiddleware)
 app.use(history())
-app.use(staticFileMiddleware)
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/index.html'))
-})
-
-//var port_number = server.listen(process.env.PORT || 3000);
-/* app.listen(3000);
-console.log( 'Express serving on ' + 3000 ) */
-/* 
-app.listen(process.env.PORT || 5000)
- */
-/* app.listen(process.env.PORT, '0.0.0.0') */
-/* 
-app.listen(5000, function () {
-  console.log( 'Express serving on 5000!' )
-}) */
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+app.use(serveStatic(__dirname + '/dist/spa'))
+app.listen(port)
