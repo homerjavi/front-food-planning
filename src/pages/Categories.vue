0dd8717ecbdc762720b1d8eb7bbdfc6153814898
JaviMY <javier@masteryield.com>
2021-09-19 22:43:47 +0200

    cssc

--- src/pages/Categories.vue
+++ src/pages/Categories.vue
@@ -98,8 +98,6 @@ export default {
 	},
 	mounted() {
 		this.getCategories();
-		console.log('process.env.ctx.dev', process.env.ctx.dev);
-		console.log('process.env.API', process.env.API);
 	},
 	methods: {
 		async getCategories(){
